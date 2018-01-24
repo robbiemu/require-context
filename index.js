841e93fc54b38499aa12d9d9ad593b1963b79001
Roberto Tomás Collins <roberto.tomas.cuentas@gmail.com>
2018-01-24 16:20:10 -0500

    multi-directory-support

--- index.js
+++ index.js
@@ -1,36 +1,41 @@
-module.exports = function(directory, recursive, regExp) {
+module.exports = function(d, recursive, regExp) {
   const dir = require('node-dir')
   const path = require('path')
 
-  let basepath =
-    directory[0] === '.'
-      ? path.resolve(__dirname + path.sep + directory)
-      : directory
-
-  try {
-    basepath = require.resolve(basepath)
-  } catch (err) {
-    if (
-      err.message.length > 18 &&
-      err.message.slice(0, 18) === 'Cannot find module'
-    ) {
-      basepath = err.message.slice(20, -1)
-    } else {
-      throw err
-    }
+  if (!Array.isArray(d)) {
+    d = [d]
   }
+  
+  let keys = []
+  d.forEach(directory => {
+    let basepath =
+      directory[0] === '.'
+        ? path.resolve(__dirname + path.sep + directory)
+        : directory
 
-  const keys = dir
-    .files(basepath, {
-      sync: true,
-      recursive: recursive || false
-    })
-    .filter(function(file) {
-      return file.match(regExp || /\.(json|js)$/)
-    })
-    .map(function(file) {
-      return '.' + path.sep + file.slice(basepath.length + 1)
-    })
+    try {
+      basepath = require.resolve(basepath)
+    } catch (err) {
+      if (
+        err.message.length > 18 &&
+        err.message.slice(0, 18) === 'Cannot find module'
+      ) {
+        basepath = err.message.slice(20, -1)
+      } else {
+        throw err
+      }
+    }
+    
+    const dirKeys = dir
+      .files(basepath, {
+        sync: true,
+        recursive: recursive || false
+      })
+      .filter(file => file.match(regExp || /\.(json|js)$/))
+      .map(file => '.' + path.sep + file.slice(basepath.length + 1))
+    keys = keys.concat(dirKeys)
+  })
+
 
   const context = function(key) {
     return require(context.resolve(key))
